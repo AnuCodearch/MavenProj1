03f02e05f87876ebc33f60001193f120c2acea95
anuCodearch <anubobhate@gmail.com>
2022-08-24 00:45:26 +0530

    Commit message - Launching code pushing

---
+++ MavenProj1/src/test/java/testClasses/Launch.java
@@ -0,0 +1,10 @@
+package testClasses;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Launch {
+public static void main(String[] args) {
+	System.setProperty("webdriver.chrome.driver", "C:\\\\\\Users\\\\\\Dell\\\\\\\\\\Downloads\\\\\\\\testerconnectionfolder\\\\\\\\\\\\chromedriver.exe");
+	ChromeDriver driver=new ChromeDriver();
+}
+}
